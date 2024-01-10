b9ddbfcceafcd3a8a32db7604decd9292c3fb5ba
aciken <adrianmarton2006@gmail.com>
2024-01-10 21:28:46 +0100

    finished plus bug

--- src/Questions.jsx
+++ src/Questions.jsx
@@ -6,14 +6,15 @@ export function Questions() {
 
 
     const handleQuestionClick = (e) => {
-
+        const questionList = document.querySelectorAll('.sign')
         e.preventDefault();
         console.log(e.target.innerText)
 
         if(e.target.innerText === '+' || e.target.innerText === '-'){
-            console.log('dadada')
-            if(e.target.innerText === '+'){
-                console.log('yea')
+            if(e.target.innerText === '+'){ 
+                questionList.forEach((li) => {
+                    li.innerText = '+';
+                })
             e.target.innerText = '-';
         }else {
             e.target.innerText = '+';
@@ -21,6 +22,9 @@ export function Questions() {
 
         }  else{
             if(e.target.children[0].innerText === '+'){
+                questionList.forEach((li) => {
+                    li.innerText = '+';
+                })
                 e.target.children[0].innerText = '-';   
         } else{
             e.target.children[0].innerText = '+';   
@@ -42,16 +46,16 @@ return(
     <div className="questions">
         
         <ul className='question-list'>
-            <li><a href="#" onClick={handleQuestionClick} >Question 1<span>+</span></a></li>
-            <li><a href="#">Question 2 <span>+</span></a></li>
-            <li><a href="#">Question 3 <span>+</span></a></li>
-            <li><a href="#">Question 4 <span>+</span></a></li>
-            <li><a href="#">Question 5 <span>+</span></a></li>
-            <li><a href="#">Question 6 <span>+</span></a></li>
-            <li><a href="#">Question 7 <span>+</span></a></li>
-            <li><a href="#">Question 8 <span>+</span></a></li>
-            <li><a href="#">Question 9 <span>+</span></a></li>
-            <li><a href="#">Question 10 <span>+</span></a></li>
+            <li><a href="#" onClick={handleQuestionClick} >Question 1<span className='sign'>+</span></a></li>
+            <li><a href="#" onClick={handleQuestionClick} >Question 2 <span className='sign'>+</span></a></li>
+            <li><a href="#" onClick={handleQuestionClick} >Question 3 <span className='sign'>+</span></a></li>
+            <li><a href="#" onClick={handleQuestionClick} >Question 4 <span className='sign'>+</span></a></li>
+            <li><a href="#" onClick={handleQuestionClick} >Question 5 <span className='sign'>+</span></a></li>
+            <li><a href="#" onClick={handleQuestionClick} >Question 6 <span className='sign'>+</span></a></li>
+            <li><a href="#" onClick={handleQuestionClick} >Question 7 <span className='sign'>+</span></a></li>
+            <li><a href="#" onClick={handleQuestionClick} >Question 8 <span className='sign'>+</span></a></li>
+            <li><a href="#" onClick={handleQuestionClick} >Question 9 <span className='sign'>+</span></a></li>
+            <li><a href="#" onClick={handleQuestionClick} >Question 10 <span className='sign'>+</span></a></li>
         </ul>
 
     </div>
