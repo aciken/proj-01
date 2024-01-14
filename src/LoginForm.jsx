e7c0ac5ec791431802f5dd5c173e64312494e0c2
aciken <adrianmarton2006@gmail.com>
2024-01-14 21:39:40 +0100

    x

--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -6,15 +6,17 @@ export function Login(props){
         <div className="login">
                 <h1>Sign In</h1>
                 <p>Sign in to your account</p>
-                <form className="login-form">
-                    <input className="login-email" type="text" placeholder="Email"/>
-                    <input type="password" placeholder="Password"/>
-                    <button>Sign In</button>
-                </form>
-                <p className="forgot">Forgot Password?</p>
-                <p className="or">OR</p>
-                <button className="google">Sign In with Google</button>
-                <p className="sign-up" onClick={props.loginClick}>Dont have an account? <a href="#">Sign Up</a></p>
+                <div className="form-border">
+                    <form className="login-form">
+                    {/* <input className="login-firstName" type="text" placeholder="First Name"/>
+                    <input className="login-lastName" type="text" placeholder="Last Name"/> */}
+                        <input className="login-email" type="text" placeholder="Email"/>
+                        <input type="password" placeholder="Password"/>
+                        <button className='sign-btn'>Sign In</button>
+                    </form>
+                    <a href='#' className="forgot">Forgot Password?</a>
+                    <p className="sign-up" onClick={props.loginClick}>Dont have an account? <a href="#">Sign Up</a></p>
+                </div>
             </div>
 
     )
